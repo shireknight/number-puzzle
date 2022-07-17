b29ee4340e6fb4f212aaea60db57b62bdb47d7fb
Ben Valentine <bvalentine@Bens-MacBook-Air.local>
2022-07-17 18:38:11 -0400

    initial commit

---
+++ src/reducer.test.js
@@ -0,0 +1,28 @@
+import reducer from './reducer';
+
+describe('reducer', () => {
+    it('should be able to move 1 down if gap below', () => {
+        let state = { items: ['1', '2', '3', null, '5', '6', '7', '8'] };
+        state = reducer(state, { type: 'move', payload: 0 });
+
+        expect(state.items).toEqual([
+            null,
+            '2',
+            '3',
+            '1',
+            '5',
+            '6',
+            '7',
+            '8',
+            '4',
+        ]);
+    });
+
+    it('should say when it is complete', () => {
+        let state = { items: ['1', '2', '3', '4', '5', '6', null, '8'] };
+        state = reducer(state, { type: 'move', payload: 8 });
+        expect(state.complete).toBe(true);
+        state = reducer(state, { type: 'move', payload: 5 });
+        expect(state.complete).toBe(false);
+    });
+});
---
+++ src/useUndoReducer.js
@@ -0,0 +1,7 @@
+import { useReducer } from 'react'
+import undo from './undo'
+
+const useUndoReducer = (reducer, initialState) =>
+    useReducer(undo(reducer), initialState)
+
+export default useUndoReducer
